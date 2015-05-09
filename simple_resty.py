ab69579f6daecb0a69e53623758b3d1ac4a9058b
tedder <ted.timmons@stansonhealth.com>
2015-05-08 22:00:04 -0700

    add another response, fix mime type

--- simple_resty.py
+++ simple_resty.py
@@ -45,7 +45,20 @@ def sms_to_email():
   conn = boto.connect_ses(profile_name='pjnet')
   send_resp = conn.send_email(email_addr, subject, body, [email_addr])
   app.logger.info('sent email. response: ' + str(send_resp))
-  return flask.Response('<Response></Response>')
+  return flask.Response('<Response></Response>', mimetype='text/xml')
+
+@app.route('/twilio/no-phone-calls-please')
+def no_phone_calls():
+  extra = request.get('extra', '')
+
+  xml = '<?xml version="1.0" encoding="UTF-8"?><Response>'
+  for x in range(2):
+    xml += '<Say voice="woman">Sorry, this number only accepts SMS text messages.</Say>'
+    if extra == 'de':
+      xml += '<Say voice="man" language="de">Sorry, akzeptiert nur diese Zahl Kurznachrichtendienst.</Say>'
+
+  xml += '</Response>'
+  return flask.Response(xml, mimetype='text/xml')
 
 def stash():
   message = client.messages.create(body='', to='', from_=config.get('twilio', 'de_phone'))
